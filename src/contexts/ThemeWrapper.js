821f2182e6e05daee193bfaf6f69de5e7289cc04
Mostafa ElGamily <mostafaabdelrahman003@gmail.com>
2024-09-25 17:09:47 +0400

    added localstorage to themeWrapper

--- src/contexts/ThemeWrapper.js
+++ src/contexts/ThemeWrapper.js
@@ -2,20 +2,24 @@ import React, { useEffect, useState } from "react";
 import themeContext from "./themeContext";
 
 const ThemeWrapper = ({ children }) => {
-  const [theme, setTheme] = useState("light");
+  const [theme, setTheme] = useState(() => {
+    return localStorage.getItem("theme") || "light";
+  });
+
   const toggleTheme = () => {
     setTheme((prevMode) => (prevMode === "light" ? "dark" : "light"));
   };
 
   useEffect(() => {
     document.documentElement.setAttribute("data-theme", theme);
+
+    localStorage.setItem("theme", theme);
   }, [theme]);
+
   return (
-    <div>
-      <themeContext.Provider value={{ toggleTheme, theme, setTheme }}>
-        {children}
-      </themeContext.Provider>
-    </div>
+    <themeContext.Provider value={{ toggleTheme, theme, setTheme }}>
+      {children}
+    </themeContext.Provider>
   );
 };
 
